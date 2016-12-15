43d98fe4e65e5ab4eda1ecc1feb945f9f9529e06
Don Knapp <don@dknapp.com>
2016-12-15 17:06:01 -0500

    they fight!  Needs more logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,16 +16,23 @@ function Fighter (pId, pName, pImg) {
     this.name = pName;
     this.img = pImg;
     this.health = Math.floor(Math.random() * 200) + 100;
-    this.attack = Math.floor(Math.random() * 60) + 1;
+    this.attack = Math.floor(Math.random() * 60) + 50;
     this.html = '<span id="' + pId + '" class="character"><img src="' + pImg + '"><span class="charactername">' + pName + '</span></span>';
     this.take_damage = function ( pFrom, pPoints ) {
         this.health -= pPoints;
         console.log( pFrom );
         console.log( pFrom.name + " inflicted damage of " + pPoints + " on" + this.name + "!" );
+    },
+    this.update_display = function() {
+        var $stats = $("#" + this.id ).parent().next("span");
+//        console.log( $stats );
+        $stats.find(".name").text( this.name );
+        $stats.find(".health").text( this.health);
+        $stats.find(".strength").text( this.attack);
     }
 }
 
-window.onload = function load() {
+window.onload = function () {
     'use strict';
 
     for ( var i = 0; i<players.length; i++ ) {
@@ -38,7 +45,6 @@ window.onload = function load() {
             chosenOne( this.getAttribute( "id" ) );
         } );
     }
-    
 };
 
 function chosenOne( selection ) {
@@ -59,7 +65,22 @@ function chosenOne( selection ) {
 
 function chosenEnemy(  ) {
     $("#myenemy").append( $("#" + myEnemyID).remove() );
+    $(".chooser .character").off("click");
     $("#audience").append( $(".chooser .character").remove() );
     console.log( "Aminmee" );
     $("#selection").animate( { height: "0px", opacity: 0} );
+    $(".stats").animate( { opacity: 1});
+    $("#" + myPlayerID).click( attack );
+    p[myPlayerID].update_display();
+    p[myEnemyID].update_display();
 }
+
+function attack() {
+    console.log( this );
+    p[myEnemyID].take_damage( p[myPlayerID], p[myPlayerID].attack);
+    p[myPlayerID].take_damage( p[myEnemyID], p[myEnemyID].attack);
+    p[myPlayerID].update_display();
+    p[myEnemyID].update_display();
+}
+
+
